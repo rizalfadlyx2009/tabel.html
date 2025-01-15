bc3cf3a78de707df4f194fd213012472dde6342a
rizalfadlyx2009 <rizalfadlyx@gmail.com>
2025-01-15 11:06:52 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,17 @@
+
+let nama = "Rizal"; 
+let alamat = "mranggen city"; 
+let usia = "15";  
+let ttl = "demak 1 juli 2009 "; 
+let gender = "laki laki"; 
+let agama = "islam"; 
+let email = "rizalfadlyx@gmail.com";
+let berat = "60";
+let tinggi = "170";
+let hobi = "scroll sosmed ";
+let cita = "pengusaha ";
+let pekerjaan = "siswa dan menjaga toko ibu ";
+let sekolah = "SMK N 1 SAYUNG";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + "tahun, saya lahir di " + ttl + "jenis kelamin saya adalah " + gender + "saya beragama " + agama + "alamat email saya adalah " + email + "saya memiliki berat badan " + berat +"kg"+ "dengan tinggi " + tinggi + "saya sangat senang " + hobi + "karena saya memiliki cita cita " + cita + "saya saat adalah seorang " + pekerjaan + "yang bersekolah di " + sekolah + "pada jurusan " + jurusan);
